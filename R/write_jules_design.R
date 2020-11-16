d7e754be9e383446b7211dd4859d7ffb6e3b23b4
dougmcneall <dougmcneall@gmail.com>
2020-11-16 11:37:04 +0000

    updating vignettes

--- R/write_jules_design.R
+++ R/write_jules_design.R
@@ -40,76 +40,76 @@ write_jules_design <- function(paramlist, n, fac, minfac, maxfac, tf, fnprefix =
     )
   )
 
-  paramvec = names(paramlist)
-  nmlvec = unlist(lapply(paramlist, FUN = function(x) x$namelist))
+  paramvec <- names(paramlist)
+  nmlvec <- unlist(lapply(paramlist, FUN = function(x) x$namelist))
 
   # which parameters do we want as a parameter list?
-  fac.ix = which(names(paramlist) %in% fac)
-  tf.ix = which(names(paramlist) %in% tf)
+  fac.ix <- which(names(paramlist) %in% fac)
+  tf.ix <- which(names(paramlist) %in% tf)
 
-  paramfac = paramlist[fac.ix]
-  paramtf = paramlist[tf.ix]
-  parampft = paramlist[-c(fac.ix, tf.ix)]
-  pftvec = names(parampft)
+  paramfac <- paramlist[fac.ix]
+  paramtf <- paramlist[tf.ix]
+  parampft <- paramlist[-c(fac.ix, tf.ix)]
+  pftvec <- names(parampft)
 
-  parampft_nml = unlist(lapply(parampft, FUN = function(x) x$namelist))
-  paramfac_nml = unlist(lapply(paramfac, FUN = function(x) x$namelist))
-  paramtf_nml = unlist(lapply(paramtf, FUN = function(x) x$namelist))
+  parampft_nml <- unlist(lapply(parampft, FUN = function(x) x$namelist))
+  paramfac_nml <- unlist(lapply(paramfac, FUN = function(x) x$namelist))
+  paramtf_nml <- unlist(lapply(paramtf, FUN = function(x) x$namelist))
 
-  parampft_standard = unlist(lapply(parampft, FUN = function(x) x$standard))
-  parampft_mins = unlist(lapply(parampft, FUN = function(x) x$min))
-  parampft_maxes = unlist(lapply(parampft, FUN = function(x) x$max))
+  parampft_standard <- unlist(lapply(parampft, FUN = function(x) x$standard))
+  parampft_mins <- unlist(lapply(parampft, FUN = function(x) x$min))
+  parampft_maxes <- unlist(lapply(parampft, FUN = function(x) x$max))
 
-  paramfac_standard = unlist(lapply(paramfac, FUN = function(x) x$standard))
-  paramtf_standard = unlist(lapply(paramtf, FUN = function(x) x$standard))
+  paramfac_standard <- unlist(lapply(paramfac, FUN = function(x) x$standard))
+  paramtf_standard <- unlist(lapply(paramtf, FUN = function(x) x$standard))
 
-  all_names = c(names(parampft_standard), fac, tf)
-  k = length(all_names)
+  all_names <- c(names(parampft_standard), fac, tf)
+  k <- length(all_names)
 
   # This writes out the 'standard' parameters, but sets the "factors"
   # used to multiply them to "1". It should produce the same
   # number of columns as the output design.
   #
-  standard_matrix = matrix( c(parampft_standard, rep(1, length(fac)), paramtf_standard), nrow = 1)
-  colnames(standard_matrix) = all_names
+  standard_matrix <- matrix( c(parampft_standard, rep(1, length(fac)), paramtf_standard), nrow = 1)
+  colnames(standard_matrix) <- all_names
   write.matrix(standard_matrix, file = stanfn)
 
-  standard_matrix_all = matrix( c(parampft_standard, paramfac_standard, paramtf_standard), nrow = 1)
+  standard_matrix_all <- matrix( c(parampft_standard, paramfac_standard, paramtf_standard), nrow = 1)
 
   # Writes standard values for all pfts, even if they aren't used.
-  colnames(standard_matrix_all) = c(names(parampft_standard),
+  colnames(standard_matrix_all) <- c(names(parampft_standard),
                                     names(paramfac_standard),
                                     names(paramtf_standard))
 
   write.matrix(standard_matrix_all, file = allstanfn)
 
   # Do the pfts and then the factors and then the logical
-  lhs = unnormalize(
+  lhs <- unnormalize(
     maximinLHS(n = n, k = k, dup = 1),
-    un.mins = c(parampft_mins , minfac, rep(0, length(tf)), recursive = TRUE),
-    un.maxes = c(parampft_maxes, maxfac, rep(1, length(tf)), recursive = TRUE)
+    un_mins <- c(parampft_mins , minfac, rep(0, length(tf)), recursive = TRUE),
+    un_maxes <- c(parampft_maxes, maxfac, rep(1, length(tf)), recursive = TRUE)
   )
-  colnames(lhs) = all_names
+  colnames(lhs) <- all_names
 
   for(i in 1:nrow(lhs)){
-    fn = paste0(fnprefix,sprintf("%04d",(startnum+i)-1),'.conf')
+    fn <- paste0(fnprefix,sprintf("%04d",(startnum+i)-1),'.conf')
 
     for(el in unique(nmlvec)){
       write(paste0('[namelist:',el,']'), file = fn, append = TRUE)
 
       # grab the parts of the list that match
-      pft_elms = parampft[parampft_nml==el] # & statement
-      pft_elms_vec = names(pft_elms)
-      fac_elms = paramfac[paramfac_nml==el]
-      fac_elms_vec = names(fac_elms)
-      tf_elms = paramtf[paramtf_nml==el]
-      tf_elms_vec = names(tf_elms)
+      pft_elms <- parampft[parampft_nml==el] # & statement
+      pft_elms_vec <- names(pft_elms)
+      fac_elms <- paramfac[paramfac_nml==el]
+      fac_elms_vec <- names(fac_elms)
+      tf_elms <- paramtf[paramtf_nml==el]
+      tf_elms_vec <- names(tf_elms)
 
       if(length(pft_elms_vec) > 0){
         for(j in 1:length(pft_elms_vec)){
-          param = pft_elms_vec[j]
-          colix = grep(param, colnames(lhs))
-          values.out = lhs[i, colix]
+          param <- pft_elms_vec[j]
+          colix <- grep(param, colnames(lhs))
+          values.out <- lhs[i, colix]
           write(paste0(param,'=',(paste0(round(values.out,rn), collapse = ',')), collapse = ''),
                 file = fn, append = TRUE)
         }
@@ -117,10 +117,10 @@ write_jules_design <- function(paramlist, n, fac, minfac, maxfac, tf, fnprefix =
 
       if(length(fac_elms_vec) > 0){
         for(k in 1: length(fac_elms_vec)){
-          param = fac_elms_vec[k]
-          colix = grep(param, colnames(lhs))
-          lhs.factor = lhs[i, colix]
-          values.out = lhs.factor * get(param, paramlist)$standard
+          param <- fac_elms_vec[k]
+          colix <- grep(param, colnames(lhs))
+          lhs.factor <- lhs[i, colix]
+          values.out <- lhs.factor * get(param, paramlist)$standard
           write(paste0(param,'=',(paste0(round(values.out,rn), collapse = ',')), collapse = ''),
                 file = fn, append = TRUE)
         }
@@ -128,10 +128,10 @@ write_jules_design <- function(paramlist, n, fac, minfac, maxfac, tf, fnprefix =
 
       if(length(tf_elms_vec) > 0){
         for(l in 1: length(tf_elms_vec)){
-          param = tf_elms_vec[l]
-          colix = grep(param, colnames(lhs))
-          lhs.factor = lhs[i, colix]
-          logical.out = NA
+          param <- tf_elms_vec[l]
+          colix <- grep(param, colnames(lhs))
+          lhs.factor <- lhs[i, colix]
+          logical.out <- NA
           if (lhs.factor>=0.5){logical.out = '.true.'}
           else {logical.out = '.false.'}
           write(paste0(param,'=',logical.out, collapse = ''), file = fn, append = TRUE)
